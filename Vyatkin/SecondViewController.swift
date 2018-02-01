fc6cd35f9c04f3098345acb7a596de055f7e9bd9
student <student@dobroeutro.ttknn.net>
2018-02-01 14:19:52 +0300

    Created 3ViewControllers

---
+++ Vyatkin/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Vyatkin
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
